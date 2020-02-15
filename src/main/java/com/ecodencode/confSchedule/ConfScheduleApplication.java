e47f080a8223817850cbc554b08429231b6ffb5d
neorusse <neorusse@gmail.com>
2020-02-15 17:04:40 +0100

    chore: Initial commit for project-setup

---
+++ src/main/java/com/ecodencode/confSchedule/ConfScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.ecodencode.confSchedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecodencode/confSchedule/ConfScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecodencode.confSchedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
