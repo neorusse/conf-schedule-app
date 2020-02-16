d7df89c210d81488321ecf17a47f3ef69abb26f9
neorusse <neorusse@gmail.com>
2020-02-16 22:48:35 +0100

    chore: Create repo for session

--- src/main/java/com/ecodencode/confSchedule/repositories/SessionRepository.java
+++ src/main/java/com/ecodencode/confSchedule/repositories/SessionRepository.java
@@ -1,4 +1,7 @@
 package com.ecodencode.confSchedule.repositories;
 
-public interface SessionRepository {
+import com.ecodencode.confSchedule.models.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
 }
