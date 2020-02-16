b0b07eaf71dda45ef488865361ec47d30b76cf17
neorusse <neorusse@gmail.com>
2020-02-16 22:51:34 +0100

    chore: Create home controller logic

--- src/main/java/com/ecodencode/confSchedule/controllers/HomeController.java
+++ src/main/java/com/ecodencode/confSchedule/controllers/HomeController.java
@@ -1,4 +1,27 @@
 package com.ecodencode.confSchedule.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class HomeController {
+
+  // injecting the app version
+  @Value("${app.version}")
+  private String appVersion;
+
+  @GetMapping
+  @RequestMapping("/")
+  public Map getStatus() {
+    Map map = new HashMap<String, String>();
+    map.put("app-version", appVersion);
+    return map;
+
+  }
+
 }
