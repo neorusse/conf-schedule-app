b9850be9341f2f0005e7830db5f4401b22ca66a3
neorusse <neorusse@gmail.com>
2020-02-16 22:50:36 +0100

    Add CRUD endpoints

--- src/main/java/com/ecodencode/confSchedule/controllers/SessionsController.java
+++ src/main/java/com/ecodencode/confSchedule/controllers/SessionsController.java
@@ -1,11 +1,58 @@
 package com.ecodencode.confSchedule.controllers;
 
+import com.ecodencode.confSchedule.models.Session;
 import com.ecodencode.confSchedule.repositories.SessionRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/sessions")
-public class SessionsContoller {
+public class SessionsController {
+
+  // wire repo to controller
+  @Autowired
   private SessionRepository sessionRepository;
+
+  // return all sessions
+  @GetMapping
+  public List<Session> list() {
+    return sessionRepository.findAll();
+  }
+
+  // get a specific session by id
+  @GetMapping
+  @RequestMapping("{id}")
+  public Session get(@PathVariable Long id) {
+    return sessionRepository.getOne(id);
+  }
+
+  // create a session
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Session create(@RequestBody final Session session) {
+    return sessionRepository.saveAndFlush(session);
+  }
+
+  // delete a session
+  @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+  public void delete(@PathVariable Long id) {
+    // Also need to check for children records before deleting
+    sessionRepository.deleteById(id);
+  }
+
+  // update/edit a session
+  @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+  public Session update(@PathVariable Long id, @RequestBody Session session) {
+    // because this is a PUT, we expect all attributes to be passed in.
+    // A PATCH would only need what you want to update
+    // TODO: Add validation that all attributes are passed-in, otherwise return a 400 bad payload
+    Session existingSession = sessionRepository.getOne(id);
+    BeanUtils.copyProperties(session, existingSession, "session_id");
+    return sessionRepository.saveAndFlush(existingSession);
+  }
+
 }
--- src/main/java/com/ecodencode/confSchedule/controllers/SpeakersController.java
+++ src/main/java/com/ecodencode/confSchedule/controllers/SpeakersController.java
@@ -1,4 +1,55 @@
 package com.ecodencode.confSchedule.controllers;
 
+import com.ecodencode.confSchedule.models.Speaker;
+import com.ecodencode.confSchedule.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
 public class SpeakersController {
+
+  // wire repo to controller
+  @Autowired
+  private SpeakerRepository speakerRepository;
+
+  // return all speakers
+  @GetMapping
+  public List<Speaker> list() {
+    return speakerRepository.findAll();
+  }
+
+  // get a specific speaker by id
+  @GetMapping
+  @RequestMapping("{id}")
+  public Speaker get(@PathVariable Long id) {
+    return speakerRepository.getOne(id);
+  }
+
+  // create a speaker
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Speaker create(@RequestBody final Speaker speaker) {
+    return speakerRepository.saveAndFlush(speaker);
+  }
+
+  // delete a speaker
+  @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+  public void delete(@PathVariable Long id) {
+    // Also need to check for children records before deleting
+    speakerRepository.deleteById(id);
+  }
+
+  // update/edit a speaker
+  @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+  public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+    // TODO: Add validation that all attributes are passed-in, otherwise return a 400 bad payload
+    Speaker existingSpeaker = speakerRepository.getOne(id);
+    BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+    return speakerRepository.saveAndFlush(existingSpeaker);
+  }
 }
